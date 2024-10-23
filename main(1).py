4d2bb7ccdcb67fe4b3fbda2c05f850f666ffb708
HarshalMhaske2023 <harshal.mhaske23@pcu.edu.in>
2024-10-23 22:33:09 +0530

    main().py

--- main(1).py
+++ main(1).py
@@ -1,5 +1,5 @@
 import mypackage
-print(mypackage.greet("ashwani"))
-print(mypackage.greet1("ashwani"))
+print(mypackage.greet("HARSHAL"))
+print(mypackage.greet1("HARSHAL"))
 print(mypackage.add(2,3))
 print(mypackage.subtract(3,4)) 
